0f979aa8cf1e3ccc586865307fd363a14d20ea9c
Tao Nguyen <taonguyen@kms.com.vn>
2023-07-06 15:05:51 +0700

    ClassesAndObjectsExercise

---
+++ src/javabasic/basicconcepts/Employee_ClassAndObject.java
@@ -0,0 +1,19 @@
+package javabasic.basicconcepts;
+
+public class Employee_ClassAndObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee_ClassAndObject taonguyen = new Employee_ClassAndObject(); // this is an object
+		
+		System.out.println("This is the first class");
+		
+		// single line comment
+		/* 
+		 *  /* space enter this is multiline comment
+		 */
+		
+	}
+
+}
