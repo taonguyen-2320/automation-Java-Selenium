8a89daca4358d81c90bb317069351563312d57a5
Tao Nguyen <taonguyen@kms.com.vn>
2023-07-07 09:04:12 +0700

    DataTypeExercise

---
+++ src/javabasic/basicconcepts/DataTypeExercise.java
@@ -0,0 +1,25 @@
+package javabasic.basicconcepts;
+
+public class DataTypeExercise {
+
+	public static void main(String[] args) {
+		// Primitive Data Types
+		byte a = 127;
+		short b = 32767;
+		int c = 764766;
+		long d = 765737L;
+		
+		float e = 1.4556675f;
+		double f = 12.5d;
+		
+		boolean g = true;
+		char h = 'c';
+		
+		// Non-Primitive Data Types
+		String str = new String();
+		
+		DataTypeExercise datatype = new DataTypeExercise();
+		
+	}
+
+}
